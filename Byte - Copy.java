120a7a7e83193f24fb2d5300aa7c6ceaabe6cca5
bhumika-ar <bhumikaarbhumi@gmail.com>
2024-07-04 10:37:42 +0530

    basics floder java code to be added

---
+++ Byte - Copy.java
@@ -0,0 +1,13 @@
+class Byte{
+ String static byte.bty;
+ String static int.i;
+  String static float.f;
+ String static long.l;
+  public static void main(String[] byte){
+      System.out.println("byte value" is: +byte);
+	  System.out.println("int value" is: +int);
+	  System.out.println("float value" is: +float);
+	  System.out.println("long value" is: +long);
+	  }
+	  }
+  
